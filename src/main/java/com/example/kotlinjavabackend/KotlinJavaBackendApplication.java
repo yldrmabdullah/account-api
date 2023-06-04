8f4a4c7f3f5a5ea3eeb5c469392a6b4239d0041c
yldrmabdullah <abdullahahmetyildirim@gmail.com>
2023-06-05 02:24:41 +0300

    first commit

---
+++ src/main/java/com/example/kotlinjavabackend/KotlinJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotlinjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kotlinjavabackend/KotlinJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotlinjavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
